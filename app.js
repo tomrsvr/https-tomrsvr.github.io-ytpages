4642286ce6261f27da7f557712127d03a5b512e5
tomrsvr <4099754+tomrsvr@users.noreply.github.com>
2022-12-08 22:30:25 +0900

    Media links, dark mode

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ function PageTransitions(){
     const themeBtn = document.querySelector('.theme-btn');
     themeBtn.addEventListener('click',() =>{
         let element = document.body;
-        element.classList.toggle('light-mode');
+        element.classList.toggle('dark-mode');
     })
 }
 
